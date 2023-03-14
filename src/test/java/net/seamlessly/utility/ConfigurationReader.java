213d91cfa94110f8061577f459d41b25389bedf9
MehmetAkkaya <mehmetakkaya1990@gmail.com>
2023-03-14 21:53:51 +0300

    initial commit

---
+++ src/test/java/net/seamlessly/utility/ConfigurationReader.java
@@ -0,0 +1,23 @@
+package net.seamlessly.utility;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigurationReader {
+
+    private static Properties properties = new Properties();
+
+    static {
+        try {
+            FileInputStream fis = new FileInputStream("configuration.properties");
+            properties.load(fis);
+            fis.close();
+        } catch (IOException e) {
+        }
+    }
+
+    public static String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+}
