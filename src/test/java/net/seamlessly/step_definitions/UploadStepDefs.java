13ce52b6b0e92ff70a010c0b54f21babeef459d8
Mustafa <mustafamuezzinoglu@gmail.com>
2023-03-29 02:33:17 +0300

    uploadStepDefs class created

---
+++ src/test/java/net/seamlessly/step_definitions/UploadStepDefs.java
@@ -0,0 +1,29 @@
+package net.seamlessly.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import net.seamlessly.pages.BasePage;
+import net.seamlessly.pages.UploadPage;
+
+public class UploadStepDefs extends BasePage {
+
+    UploadPage uploadPage = new UploadPage();
+
+    @Given("user navigate to {string} module")
+    public void user_navigate_to_module(String moduleName) {
+        uploadPage.navigateToModule(moduleName);
+    }
+
+
+    @When("user clicks the + button")
+    public void user_clicks_the_button() {
+        uploadPage.uploadButton.click();
+    }
+
+    @When("user clicks the uploadFile link")
+    public void user_clicks_the_upload_file_link() {
+        uploadPage.uploadFileLink.click();
+    }
+
+
+}
