9d36f6ef9a04d463555fb3d1ca5fd97058727e20
Mustafa Atik <mustafaatik76@gmail.com>
2023-03-29 01:07:39 +0300

    CalendarPage update

--- src/test/java/net/seamlessly/pages/CalendarPage.java
+++ src/test/java/net/seamlessly/pages/CalendarPage.java
@@ -1,4 +1,11 @@
 package net.seamlessly.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class CalendarPage {
+
+
+    @FindBy(xpath = "//span[@class='action-button__icon icon-view-day']")
+    public WebElement dayButton;
 }
