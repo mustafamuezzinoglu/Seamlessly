32599684caf3d649f61c834695cf2f5632d64b2f
Mustafa Atik <mustafaatik76@gmail.com>
2023-03-29 14:44:26 +0300

    CalendarP update

--- src/test/java/net/seamlessly/pages/CalendarPage.java
+++ src/test/java/net/seamlessly/pages/CalendarPage.java
@@ -3,9 +3,18 @@ package net.seamlessly.pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class CalendarPage {
+public class CalendarPage extends BasePage{
 
+    @FindBy(xpath = "(//div[@class='action-item'])[1]")
+    public WebElement dropdownMenu;
 
-    @FindBy(xpath = "//span[@class='action-button__icon icon-view-day']")
+    @FindBy(xpath = "(//span[@class='material-design-icon view-day-icon'])[2]")
     public WebElement dayButton;
+
+    @FindBy(xpath = "//span[@class='material-design-icon view-week-icon']")
+    public WebElement weekButton;
+
+    @FindBy(xpath = "//span[@class='material-design-icon view-module-icon']")
+    public WebElement monthButton;
+
 }
