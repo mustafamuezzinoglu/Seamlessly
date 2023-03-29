8b950b4227600fbedb3f61c2b72ddebacfc98cdb
Mustafa Atik <mustafaatik76@gmail.com>
2023-03-29 17:50:07 +0300

    CalendarStepDefs (@SEAMLES10-613) updated

--- src/test/java/net/seamlessly/step_definitions/CalendarStepDefs.java
+++ src/test/java/net/seamlessly/step_definitions/CalendarStepDefs.java
@@ -13,12 +13,14 @@ import net.seamlessly.pages.LoginPage;
 import net.seamlessly.utility.BrowserUtility;
 import net.seamlessly.utility.Driver;
 import org.junit.Assert;
+import org.openqa.selenium.WebElement;
 
 import java.util.concurrent.Callable;
 
 public class CalendarStepDefs {
 
     CalendarPage calendarPage = new CalendarPage();
+    WebElement selectedEvent;
 
 
     @When("User click on the calendar view dropdown menu")
@@ -57,4 +59,64 @@ public class CalendarStepDefs {
         Assert.assertTrue(Driver.getDriver().getCurrentUrl().contains("dayGridMonth"));
         BrowserUtility.sleep(2);
     }
+
+    @When("User  select Monthly view calendar")
+    public void user_select_monthly_view_calendar() {
+        calendarPage.dropdownMenu.click();
+        calendarPage.monthButton.click();
+    }
+
+    String time     = "7:00 PM";
+    String endTime  = "9:00 PM";
+    String date     = "2023-04-04";
+    String anyEvent ="Alumni Meeting" ;
+    String title  ;
+
+    @When("User  click on New event button")
+    public void user_click_on_New_event_button(){
+        calendarPage.newEventButton.click();
+    }
+
+    @When("User  input {string} as a new event in placeholder")
+    public void user_input_new_event_in_placeholder(String title) {
+        this.title = title;
+        calendarPage.eventTitle.sendKeys(title);
+    }
+
+    @When("User  select beginning time and date")
+    public void user_select_beginning_time_and_date() {
+        calendarPage.startTime.click();
+        calendarPage.getHour(time.substring(0,1)).click();
+        calendarPage.pm.click();
+        calendarPage.pickDateButton.click();
+        calendarPage.getPickDate(date).click();
+        calendarPage.okButton.click();
+    }
+
+    @When("User  select ending time and date")
+    public void user_select_ending_time_and_date() {
+        calendarPage.endTime.click();
+        BrowserUtility.sleep(2);
+        calendarPage.getHour(endTime.substring(0,1)).click();
+        BrowserUtility.sleep(2);
+        calendarPage.okButton.click();
+    }
+
+    @When("User  click on Save  button")
+    public void user_click_on_save_button() {
+        calendarPage.saveButton.click();
+    }
+
+    @Then("User  should see new event on monthly calendar")
+    public void user_should_see_new_event_on_monthly_calendar() {
+
+        WebElement actualHour = calendarPage.getEventTime(date, time);
+        Assert.assertEquals(time,actualHour.getText());
+
+        WebElement actualTitle = calendarPage.getEventTitle(date,title);
+        Assert.assertEquals(title,actualTitle.getText());
+
+        BrowserUtility.sleep(2);
+    }
+
 }
