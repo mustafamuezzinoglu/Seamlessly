2e4b683c2d0dbdf6df6de0136be959c275474079
Ali Karaoglan <karaoglan.a.m@gmail.com>
2023-03-29 17:05:26 +0300

    locators added

--- src/test/java/net/seamlessly/pages/FilesPage.java
+++ src/test/java/net/seamlessly/pages/FilesPage.java
@@ -1,6 +1,19 @@
 package net.seamlessly.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class FilesPage extends BasePage{
+
+
+	@FindBy(className = "actions-selected")
+	public WebElement moreIcon;
+
+	@FindBy(xpath = "//span[text()='Remove from favorites']")
+	public WebElement removeFromFavoritesBtn;
+
+	@FindBy(linkText = "Favorites")
+	public WebElement faavoritesTab;
 
 
 }
