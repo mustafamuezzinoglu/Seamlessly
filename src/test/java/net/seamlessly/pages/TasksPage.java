e95caad24050d45fff7a9f44fdf78d8af0907526
MehmetAkkaya <mehmetakkaya1990@gmail.com>
2023-03-31 16:16:33 +0300

    first AC - task list added

--- src/test/java/net/seamlessly/pages/TasksPage.java
+++ src/test/java/net/seamlessly/pages/TasksPage.java
@@ -1,4 +1,37 @@
 package net.seamlessly.pages;
 
+import net.seamlessly.utility.BrowserUtility;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class TasksPage extends BasePage{
+
+    @FindBy(xpath = "//span[normalize-space(text())='Add List…']")
+    public WebElement addList;
+
+    @FindBy(id = "newListInput")
+    public WebElement newListInput;
+
+    @FindBy(xpath = "//input[@id='newListInput']/following-sibling::input[2]")
+    public WebElement saveButton;
+
+    @FindBy(xpath = "//span[normalize-space(text())='Test List']")
+    public WebElement TestList;
+
+    @FindBy(xpath = "//span[normalize-space(text())='Test List']/../..//button[@class='icon action-item__menutoggle action-item__menutoggle--default-icon']")
+    private WebElement testListButton;
+
+    @FindBy(xpath = "//span[.='Delete']/..")
+    private WebElement testListDelete;
+
+    public void deleteTestList() {
+        testListButton.click();
+        testListDelete.click();
+    }
+
+
+
+
+
+
 }
--- src/test/java/net/seamlessly/step_definitions/TasksStepDefs.java
+++ src/test/java/net/seamlessly/step_definitions/TasksStepDefs.java
@@ -1,15 +1,50 @@
 package net.seamlessly.step_definitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.When;
 import net.seamlessly.pages.TasksPage;
+import net.seamlessly.utility.BrowserUtility;
+import static org.junit.Assert.*;
+import org.openqa.selenium.Keys;
 
 public class TasksStepDefs {
     TasksPage tasksPage = new TasksPage();
 
     @When("user navigates to {string} module")
     public void user_navigates_to_module(String moduleName) {
+        BrowserUtility.sleep(4);
         tasksPage.navigateToModule(moduleName);
+        BrowserUtility.sleep(4);
     }
 
 
+    @When("user clicks Add List…")
+    public void userClicksAddList() {
+        tasksPage.addList.click();
+    }
+
+    @And("user writes the list name {string} and presses ENTER key")
+    public void userWritesTheListNameAndPressesENTERKey(String listName) {
+        tasksPage.newListInput.sendKeys(listName + Keys.ENTER);
+        BrowserUtility.sleep(3);
+    }
+
+    @And("verify that user sees this name inside the list name list")
+    public void verifyThatUserSeesThisNameInsideTheListNameList() {
+        assertEquals(tasksPage.TestList.getText(), "Test List");
+        BrowserUtility.sleep(3);
+        tasksPage.deleteTestList();
+    }
+
+
+    @And("user writes the list name {string}")
+    public void userWritesTheListName(String listName) {
+        tasksPage.newListInput.sendKeys(listName);
+    }
+
+    @And("user clicks Save button")
+    public void userClicksSaveButton() {
+        BrowserUtility.sleep(1);
+        tasksPage.saveButton.click();
+    }
 }
