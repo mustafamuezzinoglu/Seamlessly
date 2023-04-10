583ca335ff57384d2848f39a3297462d40dc044a
Mustafa <mustafamuezzinoglu@gmail.com>
2023-04-10 23:51:31 +0300

    smoke feature created

--- src/test/java/net/seamlessly/runners/SeamlesslyRunner.java
+++ src/test/java/net/seamlessly/runners/SeamlesslyRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "net/seamlessly/step_definitions",
-        tags = "",
+        tags = "@smoke",
         stepNotifications = true,
         dryRun = false,
         publish = false
