3bc495dbed41fb0957057dbf8cdeea2d32c59383
Mustafa <mustafamuezzinoglu@gmail.com>
2023-04-17 11:39:43 +0300

    driver

--- src/test/java/net/seamlessly/utility/Driver.java
+++ src/test/java/net/seamlessly/utility/Driver.java
@@ -60,7 +60,7 @@ public class Driver {
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
                     driver.manage().window().maximize();
-                    driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+//                    driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
                     driver.manage().timeouts().pageLoadTimeout(30L, TimeUnit.SECONDS);
                     break;
                 case "chrome-headless":
